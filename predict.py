added clean_tweet_id to celan text before normalization and changed Vectorizer to Tokenizer for correct wording

predict.py
@@ -6,14 +6,16 @@ from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFacto
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 import nltk
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+import re
+import emoji
 
 @st.cache_resource
-def load_model_and_vectorizer():
+def load_model_and_tokenizer():
     model = tf.keras.models.load_model('lstm_model.keras')
-    vectorizer = joblib.load("vectorizer.pkl")
-    if not model or not vectorizer:
-        raise ValueError("Model or vectorizer not found. Please ensure they are correctly loaded.")
-    return model, vectorizer
+    tokenizer = joblib.load("tokenizer.pkl")
+    if not model or not tokenizer:
+        raise ValueError("Model or tokenizer not found. Please ensure they are correctly loaded.")
+    return model, tokenizer
 
 def uncapitalize(doc):
     return doc.lower()
@@ -23,6 +25,22 @@ stopword_factory = StopWordRemoverFactory()
 
 stopword = stopword_factory.get_stop_words()
 
+def clean_tweet_id(text):
+    text = text.lower()
+    text = emoji.demojize(text)
+    text = re.sub(r':([a-zA-Z0-9_]+):', r'\1', text)
+    text = text.replace('_', ' ')
+    text = re.sub(r'https?://\S+|www\.\S+', '', text)
+    text = re.sub(r'@\w+', '', text)
+    text = re.sub(r'#(\w+)', r'\1', text)
+    text = re.sub(r'\d+', '', text)
+    text = re.sub(r'(.)\1{2,}', r'\1\1', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+    factory = StopWordRemoverFactory()
+    stopwords = set(factory.get_stop_words())
+    text = ' '.join([word for word in text.split() if word not in stopwords])
+    
+    return text
 
 def normalize_document(doc):
     doc = uncapitalize(doc)
@@ -33,9 +51,10 @@ def normalize_document(doc):
     doc = " ".join(doc)
     return doc
 
-def predict(input_text, model, vectorizer):
+def predict(input_text, model, tokenizer):
+    input_text = clean_tweet_id(input_text)
     input_text = normalize_document(input_text)
-    X_new_seq = vectorizer.texts_to_sequences(input_text)
+    X_new_seq = tokenizer.texts_to_sequences(input_text)
     X_new_pad = pad_sequences(X_new_seq, padding='post', truncating='post')
     predictions = model.predict(X_new_pad)
     
@@ -56,8 +75,8 @@ label_categories = {
 }
 if st.button("Predict"):
     if user_input:
-        model, vectorizer = load_model_and_vectorizer()
-        predictions = predict(user_input, model, vectorizer)
+        model, tokenizer = load_model_and_tokenizer()
+        predictions = predict(user_input, model, tokenizer)
         probs = predictions[0]
         label_groups = {
             'Recyclability': ['Recyclability_positive', 'Recyclability_negative', 'Recyclability_neutral'],