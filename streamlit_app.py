711423ccca6316d09d75a6ea665ca8616b051a49
Samuel Juang <210711201@students.uajy.ac.id>
2025-07-31 10:30:02 +0700

    final revision TA

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,6 @@
 import streamlit as st
 
-data_page = st.Page("streamlit.py", title="Data Page", icon="🎈")
-machine_page = st.Page("machine.py", title="Model Training", icon="❄️")
-eval_page = st.Page("eval.py", title="Evaluation", icon="📊")
 predict_page = st.Page("predict.py", title="Prediction", icon="🎉")
-
-
 
 pg = st.navigation([predict_page])
 
